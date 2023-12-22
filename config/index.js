ec95f55549efc4ef81403882258d863043528d78
Julius Junior <30425075+Jrius4@users.noreply.github.com>
2023-12-22 14:21:54 +0300

    end-point

--- config/index.js
+++ config/index.js
@@ -1,6 +1,8 @@
+require('dotenv').config()
 const config = {
     database:{
-        url:'mongodb://localhost:27017/furniture'
+        url: `mongodb+srv://kazibwejuliusjunior:${process.env.DBPWD}@furnitureugdb.vywnezf.mongodb.net/`
+        // url:'mongodb://localhost:27017/furniture'
     },
     secretKey: '',
 }
